0937e69ca6b00abecd5f3ffc9bcfbb9f1d3a53ed
Summersby95 <james.summersby@gmail.com>
2021-04-17 16:14:01 +0100

    fix: impor json, flask, env to run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
